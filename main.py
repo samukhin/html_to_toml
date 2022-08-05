56931aa8420bc469b5803ff4ce251d1aeab2bd62
samukhin <47834890+samukhin@users.noreply.github.com>
2022-08-05 20:58:34 +0300

    first release

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import html_to_json
 import json
+import toml
 
 with open("index.html", 'r') as file:
     html_string = file.read()
 output_json = json.dumps(html_to_json.convert(html_string))
 
-print(output_json)
+#print(output_json)
+
+with open("index.json", 'r') as file:
+    input_json = json.load(file)
+output_toml = toml.dumps(input_json)
+
+print(output_toml)
