216bbd31cccb99a62930f68f8c69026b974c4619
samukhin <47834890+samukhin@users.noreply.github.com>
2022-08-05 21:07:31 +0300

    fix

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ with open("index.html", 'r') as file:
     html_string = file.read()
 output_json = json.dumps(html_to_json.convert(html_string))
 
-#print(output_json)
+with open("index.json", 'w') as file:
+    file.write(output_json)
 
 with open("index.json", 'r') as file:
     input_json = json.load(file)
 output_toml = toml.dumps(input_json)
 
-print(output_toml)
+with open("index.toml", 'w') as file:
+    file.write(output_toml)
